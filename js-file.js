7d795da2c239f3b42c236718f3b20e1069981b8b
Kailon Simon <kailonsimon@gmail.com>
2022-01-03 20:41:10 -0600

    Added endgame modal

--- js-file.js
+++ js-file.js
@@ -1,35 +1,81 @@
+let gameOver = false;
 const gameboard = (() => { //View
     const gameboardContainer = document.createElement('div');
     const mainDiv = document.querySelector('#main-div');
     gameboardContainer.id = 'gameboard-container';
     for (let i = 0; i < 9; i++) { 
-        gameboardContainer.insertAdjacentHTML('beforeend', `<div class='gameboard-item'></div>`) //creates squares within gameboard
+        gameboardContainer.insertAdjacentHTML('beforeend', `<div class='gameboard-item' data-index='${i}'></div>`) //creates squares within gameboard
     };
+    const assignSquare = (index, currentPlayer) => {
+        let selectedSquare = document.querySelector(`[data-index="${index}"]`);
+        selectedSquare.classList.add(`${currentPlayer}`);
+    }
+    const endGame = (winner) => {
+        const gameOverModal = document.createElement('div');
+        gameOverModal.classList.add('modal');
+        const gameOverModalText = document.createElement('h2');
+        gameOverModalText.textContent = `${winner.toUpperCase()} wins!`;
+        //gameOverModalText.classList.add(`${winner}-text`);
+        mainDiv.appendChild(gameOverModal);
+        gameOverModal.append(gameOverModalText);
+    }
     mainDiv.appendChild(gameboardContainer); //adds gameboard to main container
+    return {
+        assignSquare,
+        endGame
+    }
 })();
 
-const displayController = (() => { //Controller
-    let currentTurn =  'x';
-    const gameboardItems = document.querySelectorAll('.gameboard-item'); //returns array of game squares
+const gameModel = (() => { //Model/Data
+    let currentTurn = 'x';
+
     const gameboardItemValues = ['', '', '', '', '', '', '', '', '']; //array of square values; show if square is occupied
+    const _evaluateBoard = () => {
+        for (let i = 0; i < 3; i++) { //check columns
+            if (gameboardItemValues[i] && gameboardItemValues[i] == gameboardItemValues[i+3] && gameboardItemValues[i] == gameboardItemValues[i+6]) {
+                gameOver = true;
+                gameboard.endGame(currentTurn);
+            }
+        }
+        for (let i = 0; i < 7; i+=3) { //check rows
+            if (gameboardItemValues[i] && gameboardItemValues[i] == gameboardItemValues[i+1] && gameboardItemValues[i] == gameboardItemValues[i+2]) {
+                gameOver = true;
+                gameboard.endGame(currentTurn);
+            }
+        }
+        if (gameboardItemValues[4]) {
+            if (gameboardItemValues[4] == gameboardItemValues[0] && gameboardItemValues[4] == gameboardItemValues[8]) {
+                gameOver = true;
+                gameboard.endGame(currentTurn);
+            } else if (gameboardItemValues[4] == gameboardItemValues[2] && gameboardItemValues[4] == gameboardItemValues[4]) {
+                gameOver = true;
+                gameboard.endGame(currentTurn);
+            }
+        }
+    }
+    const assignSquare = (index) => {
+        if (gameboardItemValues[index] == '') {
+            gameboardItemValues[index] = currentTurn;
+            gameboard.assignSquare(index, currentTurn);
+            _evaluateBoard();
+            currentTurn == 'x' ? currentTurn = 'o' : currentTurn = 'x';
+        } else {
+            return
+        }
+    }
+    return {
+        assignSquare
+    }
+})();
+
+const displayController = (() => { //Controller/UI
+    const gameboardItems = document.querySelectorAll('.gameboard-item');
     for (let i = 0; i < gameboardItems.length; i++) { //evaluates game state with each iteration; iteration count based on number of squares
         gameboardItems[i].addEventListener('click', e => { //adds click event listener to each square
-            switch (gameboardItems[i].classList.contains('occupied')) { //evaluates if square has already been selected
-                case true: 
-                    console.log('square already occupied!');
-                    break;
-                case false: 
-                    gameboardItems[i].classList.add(`occupied`, `occupied-${currentTurn}`) //marks square as occupied
-                    gameboardItemValues[i] = currentTurn; //assigns player selection to gameboard value array
-                    console.log(gameboardItemValues);
-                    if (gameboardItemValues[0] == 'x' && gameboardItemValues[1] == 'x' && gameboardItemValues[2] == 'x') { //initial x player win state
-                        console.log(`${currentTurn} is the winner!`);
-                    } else {                
-                        currentTurn == 'x' ? currentTurn = 'o' : currentTurn = 'x'; //changes current player turn
-                    }
-                    break;
-            } 
-        })
+            if (!gameOver) {
+                gameModel.assignSquare(i);
+            }
+        });
     }
 })();
 
